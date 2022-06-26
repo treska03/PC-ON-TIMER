cb85e82e593c8756a3570da8af5ef9a8368f0326
treska03 <bartlomiej.treska@gmail.com>
2022-06-26 22:33:34 +0200

    Some fixes and progress

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import os
 import time
 from datetime import date
 
 import dbhandler
 import window
 
-threshold = 10
+threshold = 10 #IMPORT FROM CONFIG
 
+def window_loop(so_far, limit):
+    if limit > so_far:
+        time.sleep(limit - so_far)
+        
+    while True:
+        response = window.popup()
+        if response == -1:
+            os.system('shutdown -s -t 0')
+        elif response == 0:
+            break
+        else:
+            time.sleep(response*60)
 
 
 def looper(INSTANCE, before_last_activation_spent, on_day, turn_on_timestamp):
+    start_background_process = window_loop(threshold) #THREADING
     while True:
         today = date.today().strftime("%d/%m/%Y")
         if today != on_day:
@@ -16,18 +30,13 @@ def looper(INSTANCE, before_last_activation_spent, on_day, turn_on_timestamp):
             turn_on_timestamp = time.time()
             before_last_activation_spent = 0
 
+        #in seconds
         elapsed = round(before_last_activation_spent + time.time() - turn_on_timestamp, 0)
         INSTANCE.update(today, "time_spent", elapsed)
         INSTANCE.connection.commit()
         
-        if elapsed > threshold:
-            print(f"{threshold} minutes elapsed")
-            window.popup()
-        
         time.sleep(5)
 
-        while i > 0:
-            i = 1 #some_function()
 
 
 
@@ -40,6 +49,7 @@ def main():
     INSTANCE.cursor.execute("SELECT time_spent FROM Days WHERE date = :date", {"date" : today})
     time_spent_before_this_activation = INSTANCE.cursor.fetchone()[0]
 
+    window_loop(time_spent_before_this_activation, threshold*60)
     looper(INSTANCE, time_spent_before_this_activation, today, turn_on_timestamp)
 
 
@@ -48,5 +58,8 @@ main()
 
 
 
+
+
+#TURN OFF, REMIND LATER, 
 #ADD CONFIG MENU
 #ADD POPUP WINDOW
--- window.py
+++ window.py
@@ -1,30 +1,44 @@
 import tkinter as tk
 from tkinter import ttk
 
-# root window
-root = tk.Tk()
-root.geometry("400x100")
-root.title('Login')
-root.resizable(0, 0)
 
-# configure the grid
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=3)
+def popup():
+   # root window
+   root = tk.Tk()
+   root.geometry("400x100")
+   root.title('Time exceeded')
+   root.resizable(0, 0)
 
-
-def popup(n):
-   information_label = ttk.Label(root, text=f"Your device has been turned on for {n} today")
-   information_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
-
-
-popup(10)
-password_label = ttk.Label(root, text="Password:")
-password_label.grid(column=0, row=1, sticky=tk.W, padx=5, pady=5)
+   # configure the grid
+   root.columnconfigure(0, weight=1)
+   root.columnconfigure(1, weight=1)
+   
+   def information(n):
+      information_label = ttk.Label(root, text=f"Your device has been turned on for {n} today")
+      information_label.grid(column=0, row=0, sticky=tk.N, padx=5, pady=5)
 
 
-# login button
-login_button = ttk.Button(root, text="Login")
-login_button.grid(column=0, row=2, sticky=tk.E, padx=5, pady=5)
+   information(10)
+   off_button = ttk.Button(root, text="Turn OFF device")
+   off_button.grid(column=0, row=1)
 
+   stop_popup_button = ttk.Button(root, text="STOP")
+   stop_popup_button.grid(column=1, row=1)
 
-root.mainloop()
+   remind_button = ttk.Label(root, text="Remind in:")
+   remind_button.grid(column=2, row=1)
+
+   remind_entry = ttk.Entry(root,  show="*")
+   remind_entry.grid(column=3, row=1)
+
+   
+
+   def on_closing():
+      root.destroy()
+      return 
+
+   root.protocol("WM_DELETE_WINDOW", on_closing)
+   root.mainloop()
+
+if __name__ == "__main__":
+   popup()
